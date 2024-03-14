5df7e704b407797e0eb5d7758844e9f4b0307fee
HARITHIRISHA K <142714239+Harithirisha@users.noreply.github.com>
2024-03-14 06:35:40 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,20 @@
+var typedAbout = new Typed(".auto-input", {
+    strings: ["I'm Harithirisha!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
+
+var typedWork = new Typed(".auto-work", {
+    strings: ["@Psiog Digital"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
+
+var typedInterest = new Typed(".auto-interest", {
+    strings: ["A LITTLE COLOURFUL INTERESTS!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
